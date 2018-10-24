75949281fe14c518d81c8a881d495ea1159c8c32
Dorina Dumbrava <drndumbrava@gmail.com>
2018-10-24 13:48:00 +0300

    finished

---
+++ BestBooks/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestBooks.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public int Name { get; set; }
+        public int MyProperty { get; set; }
+    }
+}

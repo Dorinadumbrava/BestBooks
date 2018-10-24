5fdd935ab9a18d76e5eb9277c90c3a9e9ab523fe
Dorina Dumbrava <drndumbrava@gmail.com>
2018-10-24 17:05:01 +0300

    Added classes Author, publisher, genre and Book

--- BestBooks/Models/Book.cs
+++ BestBooks/Models/Book.cs
@@ -8,7 +8,11 @@ namespace BestBooks.Models
     public class Book
     {
         public int BookId { get; set; }
-        public int Name { get; set; }
-        public int MyProperty { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public Author AuthorId { get; set; }
+        public Publisher PublisherId { get; set; }
+        public Genre GenreId { get; set; }
+        public DateTime DatePublished { get; set; }
     }
 }
---
+++ BestBooks/Models/Publisher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestBooks.Models
+{
+    public class Publisher
+    {
+        public int PublisherID { get; set; }
+        public string Name { get; set; }
+        public string Countruy { get; set; }
+
+    }
+}

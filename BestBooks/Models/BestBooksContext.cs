2f58d34312fd41201711a756e4a5636202719562
Dorina Dumbrava <drndumbrava@gmail.com>
2018-10-25 10:43:53 +0300

    added DBContext

---
+++ BestBooks/Models/BestBooksContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BestBooks.Models
+{
+    public class BestBooksContext : DbContext
+
+    {
+        public BestBooksContext() : base()
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
